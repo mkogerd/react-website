922a855c8aac4651931c01f83bb49e06052cdf1f
Koger Darden <koger@Kogers-MacBook-Pro.local>
2022-11-19 16:26:21 -0600

    Make title section responsive

--- src/components/titleSection/TitleSection.js
+++ src/components/titleSection/TitleSection.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Grid, Typography } from '@mui/material';
 
 function TitleSection() {
   return (
@@ -11,18 +11,24 @@ function TitleSection() {
       p: 3,
       pt: 8,
     }}>
-      <Typography align="left">
-        Hi 👋, I'm Koger
-      </Typography>
-      <Typography variant="h1" align="left">
-        Full Stack Software Engineer
-      </Typography>
-      <Typography align="left">
-        I help people design and build things
-      </Typography>
-      <Box>
-        <img src="images/portrait.png" alt="Portrait of Koger" width={"100%"} />
-      </Box>
+      <Grid container>
+        <Grid item xs={12} sm={6}>
+          <Typography align="left">
+            Hi 👋, I'm Koger
+          </Typography>
+          <Typography variant="h1" align="left">
+            Full Stack Software Engineer
+          </Typography>
+          <Typography align="left">
+            I help people design and build things
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box>
+            <img src="images/portrait.png" alt="Portrait of Koger" width={"100%"} />
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
