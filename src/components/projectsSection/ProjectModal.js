0d41b3e6251db3ffb89e816953c0af628ae1af4f
Koger Darden <koger@mkogerd.com>
2022-12-06 14:03:03 -0600

    Have modal transition from sidebar to fullscreen depending on screen size

--- src/components/projectsSection/ProjectModal.js
+++ src/components/projectsSection/ProjectModal.js
@@ -32,58 +32,87 @@ function ProjectModal({ project, closeModal }) {
     }, [closeModal]);
 
     return (
-        <Modal
-            ref={modalRef}
-            variants={modalAnimationVariants}
-            initial='hidden'
-            animate='visible'
-            exit='hidden'
-        >
-            {preventBodyScroll}
-            <Button onClick={closeModal}>
-                <ArrowBack />
-                return to projects
-            </Button>
-            <TopDivider />
-            <ModalContent>
-                <Typography variant="h3">
-                    {project.name}
-                </Typography>
-                <Typography variant="body2">
-                    {project.description}
-                </Typography>
-                <ProjectImage src={project.image} alt={`"${project.name}"`} />
-                <Typography variant="h4">
-                    About
-                </Typography>
-                <Typography variant="body2">
-                    {project.summary}
-                </Typography>
-                <Typography variant="h4">
-                    Technologies
-                </Typography>
-                <Box>
-                    {project.technologies.map(tech => <Chip key={tech} label={tech} />)}
-                </Box>
-                <ButtonGroup>
-                    <Button startIcon={<GitHub />} href={project.repositoryLink} variant="outlined">
-                        View Repository
-                    </Button>
-                    { project.demoLink && 
-                        <Button startIcon={<Launch />} href={project.demoLink} variant="outlined">
-                            Check it out
+        <>
+            <ShadowOverlay
+                variants={overlayAnimationVariants}
+                initial='hidden'
+                animate='visible'
+                exit='hidden'
+            />
+            <Modal
+                ref={modalRef}
+                variants={modalAnimationVariants}
+                initial='hidden'
+                animate='visible'
+                exit='hidden'
+            >
+                {preventBodyScroll}
+                <Button onClick={closeModal}>
+                    <ArrowBack />
+                    return to projects
+                </Button>
+                <TopDivider />
+                <ModalContent>
+                    <Typography variant="h3">
+                        {project.name}
+                    </Typography>
+                    <Typography variant="body2">
+                        {project.description}
+                    </Typography>
+                    <ProjectImage src={project.image} alt={`"${project.name}"`} />
+                    <Typography variant="h4">
+                        About
+                    </Typography>
+                    <Typography variant="body2">
+                        {project.summary}
+                    </Typography>
+                    <Typography variant="h4">
+                        Technologies
+                    </Typography>
+                    <Box>
+                        {project.technologies.map(tech => <Chip key={tech} label={tech} />)}
+                    </Box>
+                    <ButtonGroup>
+                        <Button startIcon={<GitHub />} href={project.repositoryLink} variant="outlined">
+                            View Repository
                         </Button>
-                    }
-                </ButtonGroup>
-            </ModalContent>
-        </Modal>
+                        { project.demoLink &&
+                            <Button startIcon={<Launch />} href={project.demoLink} variant="outlined">
+                                Check it out
+                            </Button>
+                        }
+                    </ButtonGroup>
+                </ModalContent>
+            </Modal>
+        </>
     );
 }
+
+const ShadowOverlay = motion(styled(Box)({
+    zIndex: 3,
+    position: 'fixed',
+    height: '100%',
+    width: '100%',
+    top: 0,
+    right: 0,
+    background: 'rgba(0, 0, 0)',
+}));
+
+const overlayAnimationVariants = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 0.8,
+        transition: {
+            duration: 0.2,
+        },
+    },
+};
 
 const Modal = motion(styled(Box)(({ theme }) => ({
     position: 'fixed',
     height: `calc(100vh - ${theme.components.NavigationBar.height})`,
-    width: '100vw',
     zIndex: 3,
     top: theme.components.NavigationBar.height,
     right: 0,
@@ -91,20 +120,13 @@ const Modal = motion(styled(Box)(({ theme }) => ({
     background: theme.palette.background.default,
     textAlign: 'left',
     padding: theme.spacing(6),
+    [theme.breakpoints.down('md')]: {
+        width: '100vw',
+    },
+    [theme.breakpoints.up('md')]: {
+        width: '600px',
+    },
 })));
-
-const preventBodyScroll = <GlobalStyles styles={{ body: { overflow: 'hidden' }}} />;
-
-const ModalContent = styled(Box)(({ theme }) => ({
-    display: 'flex',
-    flexDirection: 'column',
-    rowGap: theme.spacing(2),
-}));
-
-const TopDivider = styled(Divider)(({ theme }) => ({
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-}));
 
 const modalAnimationVariants = {
   hidden: {
@@ -122,6 +144,19 @@ const modalAnimationVariants = {
     },
   },
 };
+
+const preventBodyScroll = <GlobalStyles styles={{ body: { overflow: 'hidden' }}} />;
+
+const ModalContent = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    rowGap: theme.spacing(2),
+}));
+
+const TopDivider = styled(Divider)(({ theme }) => ({
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+}));
 
 const ProjectImage = styled('img')({
     width: '100%',
